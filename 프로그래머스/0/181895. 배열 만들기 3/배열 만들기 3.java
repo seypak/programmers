f468b77f8ffe19c16e12eb61cf171b1d70a04a58
seypak <138602201+seypak@users.noreply.github.com>
2024-02-26 08:25:13 +0900

    [level 0] Title: 배열 만들기 3, Time: 2.99 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        List<Integer> list = new ArrayList<>();
+        for(int i=0; i<intervals.length; i++) {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            for(int j=start; j<=end; j++) {
+                list.add(arr[j]);
+            }
+        }
+        return list.stream().mapToInt(i->i).toArray();
+    }
+}
